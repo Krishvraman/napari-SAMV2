fe24da7f5839ef308edf41c265c65ccae9aba7ae
Krishnan Venkataraman <117613924+Krishvraman@users.noreply.github.com>
2024-10-07 15:05:58 -0500

    SAM V2 prototype code

--- src/napari_samv2/__init__.py
+++ src/napari_samv2/__init__.py
@@ -2,16 +2,8 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
-)
 
-__all__ = (
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
-)
+
+from ._widget import SAMV2_min
+
+__all__ = ("SAMV2_min",)
